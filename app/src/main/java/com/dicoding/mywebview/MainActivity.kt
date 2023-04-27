b6df1cabce1f2fb8c738a2178734435cc100452e
Musthafa Faishal <musthafafaishal@gmail.com>
2023-04-27 13:09:08 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/mywebview/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.dicoding.mywebview
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://www.dicoding.com")
+    }
+}
